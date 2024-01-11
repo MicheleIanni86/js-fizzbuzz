409b72a9e4a3487712ee51e0916092a31ade3aa1
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-11 16:24:26 +0100

    feat: Add StyleCSS

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,26 @@
+const boxContainer = document.getElementById('box-container');
 
 
 for (let i = 1; i <= 100; i++) {
-
+    // Trova numeri divisibili per 15
+    let result = "";
     if (i % 15 == 0) {
-        console.log("fizzbuzz");
+        result = "FizzBuzz";
 
+        // Trova numeri divisibili per 5
     } else if (i % 5 == 0) {
-        console.log("fizz");
+        result = "Buzz";
 
+        // Trova numeri divisibili per 3    
     } else if (i % 3 == 0) {
-        console.log("buzz");
-
+        result = "Fizz";
     }
 
     else {
-        console.log(i);
+        result = i;
     }
 
-
-}
+    const boxElement = `<div class="box ${result}">${result}</div>`;
+    boxContainer.innerHTML += boxElement;
+    console.log(result);
+};
